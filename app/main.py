901389789d84de1b3f08ee2de31b2e65e7311722
rodri <rodrigovlzsena@gmail.com>
2026-05-27 18:43:56 -0500

    middleware cabeceras http

--- app/main.py
+++ app/main.py
@@ -4,6 +4,14 @@ from app.routes import user_routes
 # Crear una instancia de la aplicación FastAPI
 app = FastAPI()
 
+# Middleware de cabeceras http
+@app.middleware("http")
+async def agregar_cabeceras(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["X-App-Name"] = "device_systems"
+    response.headers["X-API-Version"] = "1.0"
+    return response
+
 # Inclusion del router
 app.include_router(
     user_routes.router
