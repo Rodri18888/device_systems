2da21783dfec2e6e6c5d9d4733b7530862b7c4ce
rodri <rodrigovlzsena@gmail.com>
2026-05-27 10:55:10 -0500

    se añadio campo id al modelo de usuario

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,10 +1,18 @@
 from typing import Literal
 from pydantic import BaseModel, EmailStr, Field
 
+_id_counter = 0
+
 user_db = []
+
+def get_next_id() -> int:
+    global _id_counter
+    _id_counter += 1
+    return _id_counter
 
 roles = Literal['admin', 'support', 'user']
 class CrearUsuario(BaseModel):
+    id: int = Field(default_factory=get_next_id)
     name: str = Field(min_lenght=3)
     email: EmailStr
     role: roles = Field(default="user")
