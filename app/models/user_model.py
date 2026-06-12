b485c394d5e63721f7c6f24deafb1b1931fe5ca4
rodri <rodrigovlzsena@gmail.com>
2026-06-12 09:45:15 -0500

    unique=True en modelo de usuario

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,7 +7,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False, index=True)
+    email = Column(String, unique=True, nullable=False, index=True, unique=True)
     role = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.utcnow)
